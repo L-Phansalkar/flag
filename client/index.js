0561ac7c61e4f373b45ca5dc4100c4f1b9db5e08
L Phansalkar <lphansalkar@Ls-MacBook-Air.local>
2024-08-22 15:01:14 -0700

    working thru react updates that are causing issues

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
@@ -6,11 +7,17 @@ import history from './history'
 import store from './store'
 import App from './app'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
-)
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router history={history}>
+//       <App />
+//     </Router>
+//   </Provider>,
+//   document.getElementById('app')
+// )
+
+
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
